683e6c809a640c82c925e1eac08ab4e6831f0f5c
Roshan <rshnsngh00@gmail.com>
2023-04-16 03:44:00 +0100

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -8,7 +8,7 @@ sudo yum -y update
 sudo yum -y install ruby
 sudo yum -y install wget
 cd /home/ec2-user
-wget https://aws-codedeploy-eu-north-1.s3.amazonaws.com/latest/install
+wget https://aws-codedeploy-eu-west-3.s3.amazonaws.com/latest/install
 sudo chmod +x ./install
 sudo ./install auto
 
